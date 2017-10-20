f9ced110aab7ce5c125ed336e77b095313590947
Tory Herman <tory.herman12@gmail.com>
2017-10-19 21:39:49 -0500

    add removeListener function to content script

--- js/content.js
+++ js/content.js
@@ -30,9 +30,9 @@ window.__DevGadget = {
     +   "background-color: #a3e1ff !important; "
     +   "cursor: crosshair !important; "
     + "} "
-    + "div#gadget-box { "
+    + "div#dg-box { "
     +   "box-sizing: border-box !important; "
-    +   "height: 130px !important; "
+    +   "height: 128px !important; "
     +   "background-color: #000 !important; "
     +   "width: 100% !important; "
     +   "padding: 12px 20px !important; "
@@ -44,17 +44,19 @@ window.__DevGadget = {
     +   "cursor: default !important; "
     + "} "
     + "code#dg-code { "
-    +   "font-family: Monaco, Consolas, 'Andale Mono', 'DejaVu Sans Mono', monospace !important; "
+    +   "font-family: Consolas, Monaco, monospace !important; "
     +   "font-size: 12px !important; "
-    +   "white-space: pre-line; "
+    +   "white-space: nowrap !important; "
     +   "color: #b7f279 !important; "
     +   "background-color: transparent !important; "
+    +   "padding-left: 15px !important; "
+    +   "display: block !important; "
     + "} "
     + "span.dg-tag { "
     +   "color: #e468f9 !important; "
     + "} "
-    + "span.dg-prop{ "
-    +   "padding-left: 15px !important; "
+    + "span.dg-brace { "
+    +   "margin-left: -15px !important; "
     + "} "
     + "code#dg-copied { "
     +   "position: absolute !important; "
@@ -74,14 +76,14 @@ window.__DevGadget = {
 
     // move body down and insert gadget box
     var gadgetBox = document.createElement('div');
-    gadgetBox.id = 'gadget-box';
-    document.body.style.setProperty("transform", "translateY(130px)", "important");
+    gadgetBox.id = 'dg-box';
+    document.body.style.setProperty("transform", "translateY(128px)", "important");
     document.documentElement.appendChild(gadgetBox);
 
     // add mouseover, mouseout, and click listeners to all child nodes
     var nodes = document.querySelectorAll('*');
     for (var i = 0; i < nodes.length; i++) {
-      if (nodes[i].id !== "gadget-box") {
+      if (nodes[i].id !== "dg-box") {
         nodes[i].addEventListener("mouseover", this.dgMouseover);
         nodes[i].addEventListener("mouseout", this.dgMouseout);
         nodes[i].addEventListener("click", this.dgClick);
@@ -92,7 +94,7 @@ window.__DevGadget = {
   // removeBox() undoes buildBox() on disable
   removeBox: function() {
     document.body.style -= "margin-top = 100px;";
-    var gadgetBox = document.getElementById("gadget-box");
+    var gadgetBox = document.getElementById("dg-box");
     gadgetBox.parentNode.removeChild(gadgetBox);
 
     var nodes = document.querySelectorAll('*');
@@ -108,7 +110,7 @@ window.__DevGadget = {
   /**
    * dgMouseover() listener function
    * adds dg-hover class to element on mouseover
-   * appends text of relevant css styles to gadget-box div
+   * appends text of relevant css styles to dg-box div
    * @param {Event} e
    */
   dgMouseover: function(e) {
@@ -116,7 +118,7 @@ window.__DevGadget = {
     this.classList.add('dg-hover');
 
     // show hovered item in gadget box
-    var box = document.getElementById('gadget-box'),
+    var box = document.getElementById('dg-box'),
         css = window.getComputedStyle(this),
         fontFamily = css.getPropertyValue('font-family'),
         fontSize = css.getPropertyValue('font-size'),
@@ -124,11 +126,11 @@ window.__DevGadget = {
         tag = this.tagName.toLowerCase();
 
     box.innerHTML = "<code id='dg-code'>"
-    + "<span class='dg-tag'>" + tag + "</span> &#123; <br>"
-    + "<span class='dg-prop'>font-family: " + fontFamily + ";</span> <br>"
-    + "<span class='dg-prop'>font-size: " + fontSize + ";</span> <br>"
-    + "<span class='dg-prop'>color: " + color + ";</span> <br>"
-    + "&#125;"
+    + "<span class='dg-tag dg-brace'>" + tag + "</span> &#123; <br>"
+    + "font-family: " + fontFamily + "; <br>"
+    + "font-size: " + fontSize + "; <br>"
+    + "color: " + color + "; <br>"
+    + "<span class='dg-brace'>&#125;</span>"
     + "</code>";
   },
 
@@ -140,14 +142,14 @@ window.__DevGadget = {
   dgMouseout: function(e) {
     e.stopPropagation();
     this.classList.remove('dg-hover');
-    var box = document.getElementById('gadget-box');
+    var box = document.getElementById('dg-box');
     box.innerHTML = "";
   },
 
   /**
    * dgClick() listener function
    * copies displayed element styles to user clipboard
-   * appends 'copied to clipboard' to gadget-box div
+   * appends 'copied to clipboard' to dg-box div
    * @param {Event} e
    */
   dgClick: function(e) {
@@ -167,12 +169,17 @@ window.__DevGadget = {
     var copied = document.createElement('code');
     copied.id = 'dg-copied';
     copied.innerText = 'copied to clipboard!';
-    document.getElementById('gadget-box').appendChild(copied);
+    document.getElementById('dg-box').appendChild(copied);
   }
 };
 
 (function(dg) {
-  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  // remove listener if script has been executed previously
+  if (window.msgListener) {
+    chrome.runtime.onMessage.removeListener(window.msgListener);
+  }
+
+  window.msgListener = function(message, sender, sendResponse) {
     switch (message.type) {
       case "enable":
         dg.enable();
@@ -185,5 +192,7 @@ window.__DevGadget = {
     sendResponse({
       success: true
     });
-  });
+  };
+
+  chrome.runtime.onMessage.addListener(window.msgListener);
 })(__DevGadget);
