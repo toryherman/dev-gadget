f5c6f6996fd98f2438de94bfabb0b0bf763a5226
Tory Herman <tory.herman12@gmail.com>
2017-10-25 22:14:08 -0500

    fix tabs

--- js/background.js
+++ js/background.js
@@ -1,41 +1,41 @@
 "use strict";
 
 var Gadget = {
-  browserAction: function(tab) {
-    var tabId = tab.id,
-        tabTitle = tab.title,
-        tabUrl = tab.url;
+	browserAction: function(tab) {
+		var tabId = tab.id,
+				tabTitle = tab.title,
+				tabUrl = tab.url;
 
-    var args = "'action': 'loadtest'," +
-			"'loaded': window.hasOwnProperty('__DevGadget')," +
-			"'active': window.hasOwnProperty('__DevGadget') && window.__DevGadget.active";
+		var args = "'action': 'loadtest',"
+		+ "'loaded': window.hasOwnProperty('__DevGadget'),"
+		+ "'active': window.hasOwnProperty('__DevGadget') && window.__DevGadget.active";
 
 		// get the tab to send a message back to the background script telling it of the addon state
 		chrome.tabs.executeScript(tabId, {
 			code:	"chrome.runtime.sendMessage({ " + args + " });"
 		});
-  },
+	},
 
-  load: function(tabId) {
-    console.log('loading content script');
+	load: function(tabId) {
+		console.log('loading content script');
 
-    // load the script
-    chrome.tabs.executeScript(
-      tabId,
-      {
-        file:	"js/content.js"
-      },
-      function() {
-        console.log('content script for tab #' + tabId + ' has loaded');
+		// load the script
+		chrome.tabs.executeScript(
+			tabId,
+			{
+				file:	"js/content.js"
+			},
+			function() {
+				console.log('content script for tab #' + tabId + ' has loaded');
 
-        // save the tab loaded state and then load the addon
-        Gadget.enable(tabId);
-      }
-    );
-  },
+				// save the tab loaded state and then load the addon
+				Gadget.enable(tabId);
+			}
+		);
+	},
 
-  enable: function(tabId) {
-    // send message to the tab telling it to activate
+	enable: function(tabId) {
+		// send message to the tab telling it to activate
 		chrome.tabs.sendMessage(
 			tabId,
 			{
@@ -51,31 +51,31 @@ var Gadget = {
 				});
 			}
 		);
-  },
+	},
 
-  disable: function(tabId) {
-    chrome.tabs.sendMessage(
-      tabId,
-      {
-        type: 'disable'
-      },
-      function(success) {
-        console.log('disable message for tab #' + tabId + ' was sent');
+	disable: function(tabId) {
+		chrome.tabs.sendMessage(
+			tabId,
+			{
+				type: 'disable'
+			},
+			function(success) {
+				console.log('disable message for tab #' + tabId + ' was sent');
 
-        chrome.browserAction.setIcon({
-          "path": "images/icon19.png",
-          "tabId": tabId
-        });
-      }
-    );
+				chrome.browserAction.setIcon({
+					"path": "images/icon19.png",
+					"tabId": tabId
+				});
+			}
+		);
 
-    chrome.tabs.executeScript(
-      tabId,
-      {
-        file:	"js/content.js"
-      }
-    );
-  }
+		chrome.tabs.executeScript(
+			tabId,
+			{
+				file:	"js/content.js"
+			}
+		);
+	}
 };
 
 chrome.browserAction.onClicked.addListener(Gadget.browserAction);
@@ -88,7 +88,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	console.log('message: ', message);
 	console.log('sender: ', sender);
 
-  switch (message.action) {
+	switch (message.action) {
 		// check whether the addon content script is loaded and it's active state
 		case 'loadtest':
 			// content script not yet loaded
@@ -112,20 +112,20 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
 		break;
 
-    case 'disable':
-      console.log('tear down');
+		case 'disable':
+			console.log('tear down');
 
-      if (tabId) {
-        Gadget.disable(tabId);
-      }
+			if (tabId) {
+				Gadget.disable(tabId);
+			}
 
-    break;
-  }
+		break;
+	}
 
-  console.groupEnd();
-  return true;
+	console.groupEnd();
+	return true;
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
+	console.log('Command:', command);
 });
--- js/content.js
+++ js/content.js
@@ -1,220 +1,220 @@
 "use strict";
 
 window.__DevGadget = {
-  active: false,
+	active: false,
 
-  enable: function() {
-    this.buildBox();
-    console.log("enabled!");
-    this.active = true;
-  },
+	enable: function() {
+		this.buildBox();
+		console.log("enabled!");
+		this.active = true;
+	},
 
-  disable: function() {
-    this.removeBox();
-    console.log("disabled!");
-    this.active = false;
-  },
+	disable: function() {
+		this.removeBox();
+		console.log("disabled!");
+		this.active = false;
+	},
 
-  /**
-   * buildBox() defines css styles
-   * builds div at top of page to display styles of hovered elements
-   * adds listeners to all child nodes
-   */
-  buildBox: function() {
-    // inject css styles
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.id = 'dg-style-tag';
+	/**
+	 * buildBox() defines css styles
+	 * builds div at top of page to display styles of hovered elements
+	 * adds listeners to all child nodes
+	 */
+	buildBox: function() {
+		// inject css styles
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.id = 'dg-style-tag';
 
-    var classesText = ".dg-hover { "
-    +   "background-color: #a3e1ff !important; "
-    +   "cursor: crosshair !important; "
-    + "} "
-    + "div#dg-box { "
-    +   "box-sizing: border-box !important; "
-    +   "height: 128px !important; "
-    +   "background-color: #000 !important; "
-    +   "width: 100% !important; "
-    +   "padding: 12px 20px !important; "
-    +   "position: fixed !important; "
-    +   "top: 0 !important; "
-    +   "left: 0 !important; "
-    +   "z-index: 9999999 !important; "
-    +   "line-height: 20px !important; "
-    +   "cursor: default !important; "
-    + "} "
-    + "div#dg-box > code { "
-    +   "font-family: 'Source Code Pro', monospace !important; "
-    +   "font-size: 12px !important; "
-    +   "font-weight: 500 !important; "
-    +   "white-space: nowrap !important; "
-    +   "background-color: transparent !important; "
-    +   "margin: 0 !important; "
-    +   "line-height: 20px !important; "
-    +   "border: none !important; "
-    + "} "
-    + "div#dg-box > code, div#dg-box > code > span { "
-    +   "-webkit-font-smoothing: subpixel-antialiased !important; "
-    + "} "
-    + "code#dg-code { "
-    +   "color: #b7f279 !important; "
-    +   "padding: 0 0 0 15px !important; "
-    +   "display: block !important; "
-    + "} "
-    + "code#dg-copied { "
-    +   "position: absolute !important; "
-    +   "top: 12px !important; "
-    +   "right: 25px !important; "
-    +   "padding: 0 !important; "
-    +   "color: #ff6254 !important; "
-    + "} "
-    + "span.dg-tag { "
-    +   "color: #e468f9 !important; "
-    + "} "
-    + "span.dg-brace { "
-    +   "margin-left: -15px !important; "
-    + "} "
-    + "@font-face { "
-    +   "font-family: 'Source Code Pro'; "
-    +   "font-style: normal; "
-    +   "src: url('https://fonts.googleapis.com/css?family=Source+Code+Pro:500'); "
-    + "}";
+		var classesText = ".dg-hover { "
+		+   "background-color: #a3e1ff !important; "
+		+   "cursor: crosshair !important; "
+		+ "} "
+		+ "div#dg-box { "
+		+   "box-sizing: border-box !important; "
+		+   "height: 128px !important; "
+		+   "background-color: #000 !important; "
+		+   "width: 100% !important; "
+		+   "padding: 12px 20px !important; "
+		+   "position: fixed !important; "
+		+   "top: 0 !important; "
+		+   "left: 0 !important; "
+		+   "z-index: 9999999 !important; "
+		+   "line-height: 20px !important; "
+		+   "cursor: default !important; "
+		+ "} "
+		+ "div#dg-box > code { "
+		+   "font-family: 'Source Code Pro', monospace !important; "
+		+   "font-size: 12px !important; "
+		+   "font-weight: 500 !important; "
+		+   "white-space: nowrap !important; "
+		+   "background-color: transparent !important; "
+		+   "margin: 0 !important; "
+		+   "line-height: 20px !important; "
+		+   "border: none !important; "
+		+ "} "
+		+ "div#dg-box > code, div#dg-box > code > span { "
+		+   "-webkit-font-smoothing: subpixel-antialiased !important; "
+		+ "} "
+		+ "code#dg-code { "
+		+   "color: #b7f279 !important; "
+		+   "padding: 0 0 0 15px !important; "
+		+   "display: block !important; "
+		+ "} "
+		+ "code#dg-copied { "
+		+   "position: absolute !important; "
+		+   "top: 12px !important; "
+		+   "right: 25px !important; "
+		+   "padding: 0 !important; "
+		+   "color: #ff6254 !important; "
+		+ "} "
+		+ "span.dg-tag { "
+		+   "color: #e468f9 !important; "
+		+ "} "
+		+ "span.dg-brace { "
+		+   "margin-left: -15px !important; "
+		+ "} "
+		+ "@font-face { "
+		+   "font-family: 'Source Code Pro'; "
+		+   "font-style: normal; "
+		+   "src: url('https://fonts.googleapis.com/css?family=Source+Code+Pro:500'); "
+		+ "}";
 
-    if (style.styleSheet) {
-      style.styleSheet.cssText = classesText;
-    } else {
-      style.appendChild(document.createTextNode(classesText));
-    }
-    document.getElementsByTagName("head")[0].appendChild(style);
+		if (style.styleSheet) {
+			style.styleSheet.cssText = classesText;
+		} else {
+			style.appendChild(document.createTextNode(classesText));
+		}
+		document.getElementsByTagName("head")[0].appendChild(style);
 
-    // move body down and insert gadget box
-    var dgBox = document.createElement('div');
-    dgBox.id = 'dg-box';
-    document.body.style.setProperty("transform", "translateY(128px)", "important");
-    document.documentElement.appendChild(dgBox);
+		// move body down and insert gadget box
+		var dgBox = document.createElement('div');
+		dgBox.id = 'dg-box';
+		document.body.style.setProperty("transform", "translateY(128px)", "important");
+		document.documentElement.appendChild(dgBox);
 
-    // add mouseover, mouseout, and click listeners to all child nodes
-    var nodes = document.querySelectorAll('*');
-    for (var i = 0; i < nodes.length; i++) {
-      if (nodes[i].id !== "dg-box") {
-        nodes[i].addEventListener("mouseover", this.dgMouseover);
-        nodes[i].addEventListener("mouseout", this.dgMouseout);
-        nodes[i].addEventListener("click", this.dgClick);
-      }
-    };
-  },
+		// add mouseover, mouseout, and click listeners to all child nodes
+		var nodes = document.querySelectorAll('*');
+		for (var i = 0; i < nodes.length; i++) {
+			if (nodes[i].id !== "dg-box") {
+				nodes[i].addEventListener("mouseover", this.dgMouseover);
+				nodes[i].addEventListener("mouseout", this.dgMouseout);
+				nodes[i].addEventListener("click", this.dgClick);
+			}
+		};
+	},
 
-  // removeBox() undoes buildBox() on disable
-  removeBox: function() {
-    document.body.style -= "margin-top = 100px;";
-    var dgBox = document.getElementById("dg-box");
-    dgBox.parentNode.removeChild(dgBox);
+	// removeBox() undoes buildBox() on disable
+	removeBox: function() {
+		document.body.style -= "margin-top = 100px;";
+		var dgBox = document.getElementById("dg-box");
+		dgBox.parentNode.removeChild(dgBox);
 
-    var nodes = document.querySelectorAll('*');
-    for (var i = 0; i < nodes.length; i++) {
-      nodes[i].removeEventListener("mouseover", this.dgMouseover);
-      nodes[i].removeEventListener("mouseout", this.dgMouseout);
-      nodes[i].removeEventListener("click", this.dgClick);
-    };
+		var nodes = document.querySelectorAll('*');
+		for (var i = 0; i < nodes.length; i++) {
+			nodes[i].removeEventListener("mouseover", this.dgMouseover);
+			nodes[i].removeEventListener("mouseout", this.dgMouseout);
+			nodes[i].removeEventListener("click", this.dgClick);
+		};
 
-    document.getElementById('dg-style-tag').remove();
-  },
+		document.getElementById('dg-style-tag').remove();
+	},
 
-  /**
-   * dgMouseover() listener function
-   * adds dg-hover class to element on mouseover
-   * appends text of relevant css styles to dg-box div
-   * @param {Event} e
-   */
-  dgMouseover: function(e) {
-    e.stopPropagation();
-    this.classList.add('dg-hover');
+	/**
+	 * dgMouseover() listener function
+	 * adds dg-hover class to element on mouseover
+	 * appends text of relevant css styles to dg-box div
+	 * @param {Event} e
+	 */
+	dgMouseover: function(e) {
+		e.stopPropagation();
+		this.classList.add('dg-hover');
 
-    // show hovered item in gadget box
-    var box = document.getElementById('dg-box'),
-        css = window.getComputedStyle(this),
-        fontFamily = css.getPropertyValue('font-family'),
-        fontSize = css.getPropertyValue('font-size'),
-        color = css.getPropertyValue('color'),
-        tag = this.tagName.toLowerCase();
+		// show hovered item in gadget box
+		var box = document.getElementById('dg-box'),
+				css = window.getComputedStyle(this),
+				fontFamily = css.getPropertyValue('font-family'),
+				fontSize = css.getPropertyValue('font-size'),
+				color = css.getPropertyValue('color'),
+				tag = this.tagName.toLowerCase();
 
-    box.innerHTML = "<code id='dg-code'>"
-    + "<span class='dg-tag dg-brace'>" + tag + "</span> &#123; <br>"
-    + "font-family: " + fontFamily + "; <br>"
-    + "font-size: " + fontSize + "; <br>"
-    + "color: " + __DevGadget.toHex(color) + "; <br>"
-    + "<span class='dg-brace'>&#125;</span>"
-    + "</code>";
-  },
+		box.innerHTML = "<code id='dg-code'>"
+		+ "<span class='dg-tag dg-brace'>" + tag + "</span> &#123; <br>"
+		+ "font-family: " + fontFamily + "; <br>"
+		+ "font-size: " + fontSize + "; <br>"
+		+ "color: " + __DevGadget.toHex(color) + "; <br>"
+		+ "<span class='dg-brace'>&#125;</span>"
+		+ "</code>";
+	},
 
-  /**
-   * dgMouseout() listener function
-   * removes dg-hover class on mouseout
-   * @param {Event} e
-   */
-  dgMouseout: function(e) {
-    e.stopPropagation();
-    this.classList.remove('dg-hover');
-    var box = document.getElementById('dg-box');
-    box.innerHTML = "";
-  },
+	/**
+	 * dgMouseout() listener function
+	 * removes dg-hover class on mouseout
+	 * @param {Event} e
+	 */
+	dgMouseout: function(e) {
+		e.stopPropagation();
+		this.classList.remove('dg-hover');
+		var box = document.getElementById('dg-box');
+		box.innerHTML = "";
+	},
 
-  /**
-   * dgClick() listener function
-   * copies displayed element styles to user clipboard
-   * appends 'copied to clipboard' to dg-box div
-   * @param {Event} e
-   */
-  dgClick: function(e) {
-    e.preventDefault();
-    e.stopPropagation();
+	/**
+	 * dgClick() listener function
+	 * copies displayed element styles to user clipboard
+	 * appends 'copied to clipboard' to dg-box div
+	 * @param {Event} e
+	 */
+	dgClick: function(e) {
+		e.preventDefault();
+		e.stopPropagation();
 
-    var range = document.createRange(),
-        textField = document.createElement('textarea'),
-        codeText = document.getElementById('dg-code').innerText;
+		var range = document.createRange(),
+				textField = document.createElement('textarea'),
+				codeText = document.getElementById('dg-code').innerText;
 
-    textField.innerText = codeText;
-    document.body.appendChild(textField);
-    textField.select();
-    document.execCommand('copy');
-    textField.remove();
+		textField.innerText = codeText;
+		document.body.appendChild(textField);
+		textField.select();
+		document.execCommand('copy');
+		textField.remove();
 
-    var copied = document.createElement('code');
-    copied.id = 'dg-copied';
-    copied.innerText = 'copied to clipboard!';
-    document.getElementById('dg-box').appendChild(copied);
-  },
+		var copied = document.createElement('code');
+		copied.id = 'dg-copied';
+		copied.innerText = 'copied to clipboard!';
+		document.getElementById('dg-box').appendChild(copied);
+	},
 
-  toHex: function(color) {
-    var rgb = color.match(/\d+/g),
-        r = parseInt(rgb[0]).toString(16),
-        g = parseInt(rgb[1]).toString(16),
-        b = parseInt(rgb[2]).toString(16);
+	toHex: function(color) {
+		var rgb = color.match(/\d+/g),
+				r = parseInt(rgb[0]).toString(16),
+				g = parseInt(rgb[1]).toString(16),
+				b = parseInt(rgb[2]).toString(16);
 
-    return '#' + r + g + b;
-  }
+		return '#' + r + g + b;
+	}
 };
 
 (function(dg) {
-  // remove listener if script has been executed previously
-  if (window.msgListener) {
-    chrome.runtime.onMessage.removeListener(window.msgListener);
-  }
+	// remove listener if script has been executed previously
+	if (window.msgListener) {
+		chrome.runtime.onMessage.removeListener(window.msgListener);
+	}
 
-  window.msgListener = function(message, sender, sendResponse) {
-    switch (message.type) {
-      case "enable":
-        dg.enable();
-        break;
+	window.msgListener = function(message, sender, sendResponse) {
+		switch (message.type) {
+			case "enable":
+				dg.enable();
+				break;
 
-      case "disable":
-        dg.disable();
-        break;
-    }
-    sendResponse({
-      success: true
-    });
-  };
+			case "disable":
+				dg.disable();
+				break;
+		}
+		sendResponse({
+			success: true
+		});
+	};
 
-  chrome.runtime.onMessage.addListener(window.msgListener);
+	chrome.runtime.onMessage.addListener(window.msgListener);
 })(__DevGadget);
